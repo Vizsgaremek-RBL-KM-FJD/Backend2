02b888855df9f9ad71a1e79048b73f41e8892f03
Fekete János Dávid <janosdavidfekete@gmail.com>
2025-03-19 18:27:02 +0100

    initial

---
+++ services/db.js
@@ -0,0 +1,15 @@
+const mysql = require('mysql2/promise');
+const { config } = require('../config/config');
+
+async function query(sql, params) {
+    const connection = await mysql.createConnection(config.db);
+    try {
+        const [result] = await connection.execute(sql, params);
+        return result;
+    }
+    finally {
+        await connection.end()
+    }
+}
+
+module.exports = { query };
