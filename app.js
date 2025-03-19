c873710bea11a1bf18f0adc7c8601cd462e75918
Fekete János Dávid <janosdavidfekete@gmail.com>
2025-03-19 19:01:24 +0100

    basics

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const usersRouter = require("./routes/users");
+const port = 3000;
+
+const app = express();
+
+app.use(cors(
+    {
+        origin: ["http://localhost:4200", "https://localhost:4201"],
+        credentials: true
+    }
+));
+
+app.use(bodyParser.json());
+
+app.use("/users", usersRouter);
+
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
+app.use((err, req, res, next) => {
+    console.log(err.message, err.stack);
+    res.status(err.statusCode || 500).json({error: err.message});
+    return;
+})
+
+app.listen(port, () => {
+    console.log(`Szerver fut a http://localhost:${port} címen`);
+});
